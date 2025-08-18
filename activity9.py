1fc96c91afdf280e53710ff5993623035c4f6b5a
Narumi-4school <dwyaneestilo96@gmail.com>
2025-08-18 14:21:23 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,14 @@
+a = 3
+b = 4
+
+c = 2
+d = 5
+
+username = 'dash'
+password = 'storm'
+
+#print ("USER LOGIN")
+#print( (username == 'dash') and (password == 'storm') )
+
+print ( (a>b) and (c<d) )
+print ( (a>b) or (c<d) )
