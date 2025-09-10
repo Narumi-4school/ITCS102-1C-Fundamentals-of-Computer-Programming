1c83fbedaf478e4eb4b7345ef2b99a87410374c0
Narumi-4school <dwyaneestilo96@gmail.com>
2025-09-10 18:25:40 +0800

    Create code_challenge4.py

---
+++ code_challenge4.py
@@ -0,0 +1,12 @@
+print ("Konnichiwa, Welcome to 1Cmangareader.com")
+genre = input("Select genre (action/horror/romance): ")
+year = input("Select year (2000/2010): ")
+length = input("Select desired length (short/medium/long): ")
+genre_choice = genre
+year_choice = year
+length_choice = length
+
+if genre_choice == 'action' and year_choice == ' 2000' and length_choice == 'long':
+        print("Hi i would recommend you to read Naruto")
+else:  
+        print("Hello we dont have any manga thats",genre_choice year_choice length_choice,")
