85fa9838f761d12ff82236fea1eed47edf5a0531
Dwyane Estilo <dwyaneestilo96@gmail.com>
2025-11-12 14:06:28 +0800

    Add files via upload

---
+++ activity23.py
@@ -0,0 +1,12 @@
+from activity23_1 import GreetWithName, GreetPerson, FunctionWithReturn, Factorial
+#Defining a function
+def MyFirstFunction():
+    print("Hello Player")
+    print("Welcome to the battlefield")
+
+MyFirstFunction()   
+GreetWithName('Narumi')
+GreetWithName('Gen')
+GreetPerson('Dash', 'Lucena', '19')
+print(f"I want to get the summation of {FunctionWithReturn(10)}")
+print(f"The factorial of {Factorial(6)}")
