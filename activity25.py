f72bef11ed51fdff32820a11e48c311e84cb6626
Dwyane Estilo <dwyaneestilo96@gmail.com>
2025-11-17 14:30:07 +0800

    Add files via upload

---
+++ activity25.py
@@ -0,0 +1,16 @@
+#LIST
+
+programming_language = ['java', 'C#', 'C++', 'C', 'Perl', 'Python', 'JavaScript', 'Rust', 'GoLang']
+
+print(programming_language)
+
+#DICTIONARY
+
+emtpy_dictionary = {} #empty dictionary
+# { KEY  VALUE } : dictionary has no index, it uses 'KEYS'
+
+pl_dictionary = {'balane' : 'java', 'lance' : 'C#', 'pencerga' : 'C++',\
+                  'ebreo' : 'C', 'arandela' : 'Perl', 'enrile' : 'Python',\
+                      'dimaano' : 'JavaScript', 'ganac' : 'Rust', 'estilo' : 'GoLang'}
+
+print(pl_dictionary['balane'])                         
